273b0eb41887be76116c5efd5e884702cf825952
devananda-pk <271501838+devananda-pk@users.noreply.github.com>
2026-03-27 23:45:55 +0530

    Revert "[23:14] Add backend routes, controllers, and middlewares"

--- server/routes/report.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getReport } = require('../controllers/report.controller');
-
-// GET /api/report?location=CityName
-router.get('/', getReport);
-
-module.exports = router;
--- server/routes/risk.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getRiskPrediction } = require('../controllers/risk.controller');
-
-// GET /api/risk?location=CityName
-router.get('/', getRiskPrediction);
-
-module.exports = router;
